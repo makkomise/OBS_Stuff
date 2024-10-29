567495eb96640b739b6b198ba56c4648cd501077
Mikko Turunen <57153670+makkomise@users.noreply.github.com>
2024-10-29 14:56:20 +0200

    Update tuna_browser.user.js

--- tuna_browser.user.js
+++ tuna_browser.user.js
@@ -5,7 +5,6 @@
 // @description  Get song information from web players, based on NowSniper by Kıraç Armağan Önal
 // @author       univrsal - fork by makkbert
 // @match        *://music.youtube.com*
-// @match        *://youtube.com/*
 // @grant        unsafeWindow
 // @license      GPLv2
 // ==/UserScript==
@@ -90,84 +89,10 @@
 
             let hostname = window.location.hostname;
             // TODO: maybe add more?
-            if (hostname === 'soundcloud.com') {
-                let status = query('.playControl', e => e.classList.contains('playing') ? "playing" : "stopped", 'unknown');
-                let cover = query('.playbackSoundBadge span.sc-artwork', e => e.style.backgroundImage.slice(5, -2).replace('t50x50', 't500x500'));
-                let title = query('.playbackSoundBadge__titleLink', e => e.title);
-                let artists = [query('.playbackSoundBadge__lightLink', e => e.title)];
-                let progress = query('.playbackTimeline__timePassed span:nth-child(2)', e => timestamp_to_ms(e.textContent));
-                let duration = query('.playbackTimeline__duration span:nth-child(2)', e => timestamp_to_ms(e.textContent));
-                let album_url = query('.playbackSoundBadge__titleLink', e => e.href);
-                let album = null;
-                // this header only exists on album/set pages so we know this is a full album
-                album = query('.fullListenHero .soundTitle__title', e => {
-                    album_url = window.location.href;
-                    return e.innerText
-                })
-
-                album = query('div.playlist.playing', e => {
-                    return e.getElementsByClassName('soundTitle__title')[0].innerText;
-                })
-
-                if (title !== null) {
-                    post({ cover, title, artists, status, progress, duration, album_url, album });
-                }
-            } else if (hostname === 'music.yandex.ru') {
-                // Yandex music support by MjKey
-                let status = query('.player-controls__btn_play', e => e.classList.contains('player-controls__btn_pause') ? "playing" : "stopped", 'unknown');
-                let cover = query('.track-cover .entity-cover__image', e => e.src.replace('50x50', '200x200'));
-                let title = query('.track__title', e => e.title);
-                let artists = [query('.track__artists', e => e.textContent)];
-                let progress = query('.progress__left', e => timestamp_to_ms(e.textContent));
-                let duration = query('.progress__right', e => timestamp_to_ms(e.textContent));
-                let album_url = query('.track-cover a', e => e.title);
-
-                if (title !== null) {
-                    post({ cover, title, artists, status, progress, duration, album_url });
-                }
-            } else if (hostname === 'www.youtube.com') {
-                if (!navigator.mediaSession.metadata) // if nothing is playing we don't submit anything, otherwise having two youtube tabs open causes issues
+            if (hostname === 'music.youtube.com') {
+                if (!navigator.mediaSession.metadata) { // if nothing is playing we don't submit anything, otherwise having two youtube tabs open causes issues
                     return;
-                let artists = [];
-
-                try {
-                    artists = [document.querySelector('div#upload-info').querySelector('a').innerText.trim().replace("\n", "")];
-                } catch (e) { }
-
-                let title = query('.style-scope.ytd-video-primary-info-renderer', e => {
-                    let t = e.getElementsByClassName('title');
-                    if (t && t.length > 0)
-                        return t[0].innerText;
-                    return "";
-                });
-                let duration = query('video', e => e.duration * 1000);
-                let progress = query('video', e => e.currentTime * 1000);
-                let cover = "";
-                let status = query('video', e => e.paused ? 'stopped' : 'playing', 'unknown');
-                let regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-                let match = window.location.toString().match(regExp);
-                if (match && match[2].length == 11) {
-                    cover = `https://i.ytimg.com/vi/${match[2]}/maxresdefault.jpg`;
                 }
-
-
-                if (title !== null) {
-                    title = title.replace(`${artists.join(", ")} - `, "");
-                    title = title.replace(` - ${artists.join(", ")}`, "");
-                    title = title.replace(`${artists.join(", ")}`, "");
-                    title = title.replace("(Official Audio)", "");
-                    title = title.replace("(Official Music Video)", "");
-                    title = title.replace("(Original Video)", "");
-                    title = title.replace("(Original Mix)", "");
-                    if (status !== 'stopped') {
-                        post({ cover, title, artists, status, progress: Math.floor(progress), duration });
-                    } else {
-                        post({ status: 'stopped', title: '', artists: [], progress: 0, duration: 0 });
-                    }
-                }
-            } else if (hostname === 'music.youtube.com') {
-                if (!navigator.mediaSession.metadata) // if nothing is playing we don't submit anything, otherwise having two youtube tabs open causes issues
-                    return;
                 // Youtube Music support by Rubecks
                 const artistsSelectors = [
                     '.ytmusic-player-bar.byline [href*="channel/"]:not([href*="channel/MPREb_"]):not([href*="browse/MPREb_"])', // Artists with links
@@ -196,29 +121,26 @@
                 if (title !== null) {
                     post({ cover, title, artists, status, progress, duration, album_url, album });
                 }
-            } else if (hostname === 'www.deezer.com') {
-                let status = query('.chakra-button.css-h1gi0s', e => {
-                    return e.getAttribute('aria-label').toLowerCase() === "play" ? "paused" : "playing";
-                }, "stopped");
-
-                if ("mediaSession" in navigator && navigator.mediaSession.metadata !== null) {
-                    let data = navigator.mediaSession;
-                    let album = data.metadata.album;
-                    let res = data.metadata.artwork[0].sizes;
-                    let cover = data.metadata.artwork[0].src.replace(res, '512x512');
-                    let title = data.metadata.title
-                    let artists = data.metadata.artist.split(",").map(x => x.trim());
-                    let progress_input = document.querySelector('input.slider-track-input.mousetrap');
-                    let progress = Math.round(progress_input.value * 1000);
-                    let duration = Math.round(progress_input.max * 1000);
-                    if (title !== null) {
-                        post({ cover, title, artists, status, progress, duration, album });
-                    }
-                }
 			}
         }, refresh_rate_ms);
 
     }
 
     StartFunction();
+})();
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-29
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/makkomise/OBS_Stuff/blob/main/tuna_browser.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
 })();
